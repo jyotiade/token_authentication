cea5faf4b37bb7338c5edcbbb350d01d77b679c2
jyoti ade <jyotiade287@gmail.com>
2024-08-12 23:07:31 +0530

    token

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 from rest_framework import routers 
 from .views import MovieViewSet
-  
+from rest_framework.authtoken import views 
 # define the router
 router = routers.DefaultRouter()
 router.register(r'movie', MovieViewSet, basename='movie')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
     # path('api-auth/', include('rest_framework.urls')) 
 ]
